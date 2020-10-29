91c3429d92a9924eb5143f8efcb6f7b1db72867e
Julio Pinheiro <juliorenan@live.com>
2020-10-28 21:11:41 -0300

    feat: Added the header, pokemon and footer components

--- SPA/pokedex/src/app/app.module.ts
+++ SPA/pokedex/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { PokemonComponent } from './pokemon/pokemon.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    PokemonComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
