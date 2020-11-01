d2e26a429256a91d5014f537b20dbf16feb4b9ae
Julio Pinheiro <juliorenan@live.com>
2020-11-01 20:36:39 -0300

    fix: commenting some unused codes

--- SPA/pokedex/src/app/pokemon/pokemon.component.ts
+++ SPA/pokedex/src/app/pokemon/pokemon.component.ts
@@ -7,11 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PokemonComponent implements OnInit {
 
+
   nomePokemon: string;
   tipoPokemon: string;
   descricaoPokemon: string;
 
   constructor() {
+  // is this the best way to get pokemons?
     this.nomePokemon = 'Suicune - スイクン';
     this.tipoPokemon = 'Water';
     this.descricaoPokemon = 'Pokemon Baum';
@@ -21,11 +23,11 @@ export class PokemonComponent implements OnInit {
   }
 
   recuperarFoto() {
-    return 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/245.png';
+    // return 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/245.png';
   }
 
   recuperarSprite(){
-    return 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/245.png';
+    // return 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/245.png';
   }
 
 
